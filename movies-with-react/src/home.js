59013cda5f8a48f61e5c84c36f6bd1c96bba80d2
Shahram Ghassemi <shahramg92@hotmail.com>
2018-06-12 20:27:31 -0500

    finish headers

---
+++ movies-with-react/src/home.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Header from './components/Header/Header';
+import Form from './components/Form/Form';
+import NewMovies from './components/NewMovies/NewMovies';
+
+class Home extends Component {
+  render() {
+    return(
+      <div className="container">
+        <Header />
+        <Form />
+        <NewMovies />
+      </div>
+    );
+  }
+}
+
+export default Home;
--- movies-with-react/src/index.js
+++ movies-with-react/src/index.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './home';
+import Movie from './components/Movie/Movie';
 import './index.css';
-import App from './App';
 
+class App extends Component {
+  render() {
+    console.log('Made by Shahram Ghassemi - http://www.shahramghassemi.com/');
+    return(
+      <BrowserRouter>
+        <Switch>
+          <Route path="/movie/:id" component={Movie} />
+          <Route path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
+}
 
-ReactDOM.render( App, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
